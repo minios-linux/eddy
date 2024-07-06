997163efaf74aa8eea1e0619f0868064fceaa7bb
crim50n <crims0n@yandex.ru>
2024-07-06 18:07:18 +0300

    Update 20240706

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define variables
+REPO=https://github.com/donadigo/eddy
+NAME=com.github.donadigo.eddy
+VERSION=1.3.2
+
+# Convert NAME to lower case
+PACKAGE_NAME=$(echo "$NAME" | tr '[:upper:]' '[:lower:]')
+
+# Remove 'v' or 'V' if it is the first character
+if [[ $VERSION == [vV]* ]]; then
+    PACKAGE_VERSION="${VERSION:1}"
+else
+    PACKAGE_VERSION="$VERSION"
+fi
+
+# Clone Upstream using NAME
+git clone $REPO/$NAME -b $VERSION
+mv ./eddy ./$PACKAGE_NAME-$PACKAGE_VERSION
+cp -rvf ./debian ./$PACKAGE_NAME-$PACKAGE_VERSION
+cd ./$PACKAGE_NAME-$PACKAGE_VERSION
+
+# Create orig archive
+tar czvf ../${PACKAGE_NAME}_${PACKAGE_VERSION}.orig.tar.gz .
+
+# Get build dependencies
+apt-get build-dep . -y
+
+# Build package
+dpkg-buildpackage -uc -us
